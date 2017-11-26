ee506ca0071247f314e52d3598527ab2b3084dcb
Dmitry Ledentsov <dmlled@yahoo.com>
2017-11-26 21:38:56 +0100

    c++ round-trip test

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # risgo
 
+a simple cross-platform C++ resource compiler
+> embed data / strings as constants in source
+
 [![Build Status](https://travis-ci.org/d-led/risgo.svg?branch=master)](https://travis-ci.org/d-led/risgo)
 
+## credits
+
+- [spf13/cobra](https://github.com/spf13/cobra) CLI implementation
+- [go-yaml/yaml](https://github.com/go-yaml/yaml) YAML parsing/rendering
+- [aymerick/raymond](https://github.com/aymerick/raymond) Handlebars (templates) for output customization
+- [onqtam/doctest](https://github.com/onqtam/doctest) C++ tests [examples/doctest.h](examples/doctest.h)
--- examples/test.cpp
+++ examples/test.cpp
@@ -1,11 +1,44 @@
 #ifndef RES_INCLUDE
-#include "resource.h"
-#else
-#include RES_INCLUDE
+#define RES_INCLUDE "resource.h"
 #endif
 
-#include <iostream>
+#include RES_INCLUDE
 
-int main() {
-    std::cout<<test::res::multiline_example()<<std::endl;
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace {
+std::string readAllBytes(std::string const& filename)
+{
+    std::ifstream file(filename, std::ios::binary);
+    if (!file)
+        throw std::runtime_error(std::string("cannot open ") + filename);
+
+    return std::string(std::istreambuf_iterator<char>(file),
+                       std::istreambuf_iterator<char>());
+}
+}
+
+TEST_CASE("files are embedded exactly")
+{
+    std::cout<<"Used header: " RES_INCLUDE<<std::endl;
+    CHECK(readAllBytes("examples/test.yml") == test::res::itself());
+}
+
+TEST_CASE("getting strings via custom member names and getters")
+{
+    CHECK(test::res::Get("custom.member/name") == test::res::custom_member_name());
+    CHECK(test::res::custom_member_name().size() > 0);
+}
+
+TEST_CASE("files are not null-terminated (test.bin should be <<0,1,2,3>>)") {
+    std::string binary(test::res::binary_file_test());
+    CHECK(readAllBytes("examples/test.bin") == binary);
+    CHECK(binary.size()>1);
+    // by design of the test file
+    CHECK(binary[0]==0);
 }
