94705b40adbcf6ba80fad13de17abb11afc50695
Dmitry Ledentsov <dmlled@yahoo.com>
2017-11-26 22:29:42 +0100

    windows build & test

--- make.bat
+++ make.bat
@@ -7,10 +7,21 @@ go build
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 risgo.exe examples/test.yml
-rem g++ examples/test.cpp examples/resource.cpp -Iexamples -I. -std=c++11 -o test
-rem ./test
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cl examples\test.cpp examples\resource.cpp /EHsc /Iexamples /I. /Fetest
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+test.exe
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
 
 @rem testing backslashes
 risgo.exe examples\test.yml --template examples\cpp03.yml --header examples\resource03.h --source examples\resource03.cpp
-rem g++ examples/test.cpp examples/resource03.cpp -DRES_INCLUDE=\"resource03.h\" -Iexamples -I. -std=c++03 -o test03
-rem ./test03
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cl examples\test.cpp examples\resource03.cpp /EHsc /DRES_INCLUDE=\"resource03.h\" /Iexamples /I. /Fetest03
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+test03.exe
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
